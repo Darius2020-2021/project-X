8e58ad2157e6e387be61b21983ca4ff54093a228
Darius2020-2021 <76709772+Darius2020-2021@users.noreply.github.com>
2021-01-01 10:42:06 +0000

    Create speack_bot.py

---
+++ speack_bot.py
@@ -0,0 +1,5 @@
+from random import choice
+bonjour= ["bonjour comment allez vous ?", "bien le bonjour ", "bonjour monsieur", "comment allez vous ?", "ça va ?" ]
+bonjour_area = choice(bonjour)
+reponse_bonjour= ["super alors ", "super", "cool"]
+reponse_bonjour_area= choice(reponse_bonjour)
